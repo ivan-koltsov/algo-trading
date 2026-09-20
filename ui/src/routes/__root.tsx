b927f100f72fbbb159e9d4624bd5947be3a3b014
Ivan Koltsov <ivan.koltsov.777@gmail.com>
2026-09-20 19:11:19 +0300

    feat: add SVG favicon and configure site icons in root route

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -24,6 +24,15 @@ export const Route = createRootRoute({
     ],
     links: [
       {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        href: '/favicon.svg',
+      },
+      {
+        rel: 'apple-touch-icon',
+        href: '/favicon.svg',
+      },
+      {
         rel: 'stylesheet',
         href: appCss,
       },
